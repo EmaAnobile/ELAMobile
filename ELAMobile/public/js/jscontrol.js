76cf907dcec41c7162a0068e119dc08431161a26
EmaAnobile <emanuel.anobile@gmail.com>
2016-07-02 22:10:45 -0300

    Update jscontrol.js

--- ELAMobile/public/js/jscontrol.js
+++ ELAMobile/public/js/jscontrol.js
@@ -6,6 +6,7 @@ function SelGroup() {
       data: $(this).serialize(),
       //Enviamos para mostrar un mensaje al precionar el botón
       success: function (data) {
+          alert(lv_boton)
         if (data == "true") {
           alert("Mamaa Mia!!")
         }
